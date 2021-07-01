d96522912a4e0f0b19999d962cb276684fb3e103
sequeirab <sequeirab86@gmail.com>
2021-07-01 14:00:33 +0200

    backup

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.get('/', (req, res) => {
     res.send(database.users);
 })
 
-app.post('/signin', (req,res) => {
+app.post('/signin', (req, res) => {
        
     if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json('signin');
+        res.json("success");
     } else {
         res.status(400).json('error logging in');
     }
@@ -87,7 +87,7 @@ app.put('/image', (req, res) => {
     }
 })
 
-app.listen(3001, () => {
-    console.log('app is running on port 3001')
+app.listen(3000, () => {
+    console.log('app is running on port 3000')
 });
 
